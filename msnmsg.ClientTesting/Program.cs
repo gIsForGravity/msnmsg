fadae503f9a536b69f5f310e44d32253aeb0ff1c
gIsForGravity <gavin.tantleff@pm.me>
2023-09-08 23:21:54 -0400

    it's jover

---
+++ msnmsg.ClientTesting/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using msnmsg.Common;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5151");
+var client = new Greeter.GreeterClient(channel);
+    
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
