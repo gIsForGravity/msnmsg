d345990f7ff98834c586468e4e5eadb061888b42
Finnerex <finnerex@gmail.com>
2023-09-09 11:48:13 -0400

    comit mesagce

--- msnmsg.ClientTesting/Program.cs
+++ msnmsg.ClientTesting/Program.cs
@@ -4,19 +4,35 @@ using System.Net.Security;
 using Grpc.Net.Client;
 using msnmsg.Protocol;
 
+const string CLIENT_USERNAME = "ClientUser";
+
 Console.WriteLine("connecting to server");
-using var channel = GrpcChannel.ForAddress("http://10.251.142.210:5151");
+using var channel = GrpcChannel.ForAddress("http://tantleffbeef.co:5151");
 var client = new MsnMsgServer.MsnMsgServerClient(channel);
 
-// var serverMsgStream = client.OpenStream(new OpenStreamArgs());
+var serverMsgStream = client.OpenStream(new OpenStreamArgs());
 
-Console.WriteLine("sending message");
-await client.SendMessageAsync(new MessageInfo
+while (true)
 {
-    Message = "you like peen",
-    Name = "balls"
-});
-Console.WriteLine("message sent");
+    string? msg = Console.ReadLine();
+
+    if (msg != null)
+    {
+        Console.WriteLine("sending message: " + msg);
+        client.SendMessage(new MessageInfo
+        {
+            Message = msg,
+            Name = CLIENT_USERNAME
+        });
+        
+        Console.WriteLine("Sent!");
+    }
+
+    MessageInfo currentStream = serverMsgStream.ResponseStream.Current;
+    Console.WriteLine(currentStream.Name + ":\n" + currentStream.Message);
+
+}
+
 
 // var reply = await client.SayHelloAsync(
 //     new HelloRequest { Name = "GreeterClient" });
