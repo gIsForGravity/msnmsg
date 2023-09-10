08b0dd75339747e83c8579e56d527611d29a1c2d
Finnerex <finnerex@gmail.com>
2023-09-09 21:48:48 -0400

    camges

--- msnmsg.Client/MainPage.xaml.cs
+++ msnmsg.Client/MainPage.xaml.cs
@@ -1,23 +1,32 @@
 ﻿namespace msnmsg.Client;
 
+using Grpc.Core;
+using Grpc.Net.Client;
+using msnmsg.Protocol;
+
 public partial class MainPage : ContentPage
 {
-    int count = 0;
 
+    private ClientBase<MsnMsgServer.MsnMsgServerClient> _client;
+    
     public MainPage()
     {
+        using var channel = GrpcChannel.ForAddress("http://192.168.137.1:5151");
+        _client = new MsnMsgServer.MsnMsgServerClient(channel);
         InitializeComponent();
     }
-
-    private void OnCounterClicked(object sender, EventArgs e)
+    
+    void OnEntryCompleted(object sender, EventArgs e)
     {
-        count++;
+        Entry entry = sender as Entry;
+        string text = entry.Text;
+        entry.Text = "";
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
+        AddMessage(text);
+    }
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
+    private void AddMessage(string msg)
+    {
+        MessageBox.Text += msg + "\n";
     }
 }
